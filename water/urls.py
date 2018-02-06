6abec94aa4be80c91fe561649f8352189b68066d
apengok <apengok@163.com>
2018-02-06 17:50:56 +0800

    add xu provider

--- water/urls.py
+++ water/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from . import views
 from django.views.generic import TemplateView
 
+from . import views
+
 app_name = 'water'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-
+    url(r'dma_online/',views.dma_online,name='dma_online'),
+    url(r'^$', views.WaterListview.as_view()),
 ]
--- water/views.py
+++ water/views.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView,FormView
 
 # Create your views here.
-def index(request):
-    return 'Water page'
+def dma_online(request):
+    return render(request,"water/dma_online.html",{})
+
+
+
+class WaterListview(ListView):
+
+    template_name = 'water/water_home.html'
+
+    def get_queryset(self):
+        return None
+
+    def get_context_data(self, *args, **kwargs):
+        
+        context = super(WaterListview, self).get_context_data(*args, **kwargs)
+        
+        return context
